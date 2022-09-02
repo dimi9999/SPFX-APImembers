43011f138e52cb68f64cce03c42bce38d826e61e
dimi9999 <33888955+dimi9999@users.noreply.github.com>
2022-09-02 16:52:50 +0100

    Add files via upload

---
+++ lib/webparts/helloWorld/components/HelloWorld.d.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { IHelloWorldProps } from './IHelloWorldProps';
+export declare const PersonaBasicExample: React.FunctionComponent;
+export default class HelloWorld extends React.Component<IHelloWorldProps, {}> {
+    render(): React.ReactElement<IHelloWorldProps>;
+}
+//# sourceMappingURL=HelloWorld.d.ts.map
